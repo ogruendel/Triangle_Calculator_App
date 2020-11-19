fb6d37574d9ebfa30a186e4a2cc1e7447dc53c8d
oliver <oliver.gruendel2004@gmail.com>
2020-11-19 22:31:58 +0100

    Added Textviews to show the calculated sides and angles.

--- app/src/main/java/me/ogruendel/trianglecalculator/MainActivity.java
+++ app/src/main/java/me/ogruendel/trianglecalculator/MainActivity.java
@@ -2,17 +2,23 @@ package me.ogruendel.trianglecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab_calc = findViewById(R.id.fab_calc_missing);
 
-        EditText et_angle_alpha = findViewById(R.id.et_angle_alpha);
-        EditText et_angle_beta = findViewById(R.id.et_angle_beta);
-        EditText et_angle_gamma = findViewById(R.id.et_angle_gamma);
         EditText et_side_a = findViewById(R.id.et_side_a);
         EditText et_side_b = findViewById(R.id.et_side_b);
         EditText et_side_c = findViewById(R.id.et_side_c);
+        EditText et_angle_alpha = findViewById(R.id.et_angle_alpha);
+        EditText et_angle_beta = findViewById(R.id.et_angle_beta);
+        EditText et_angle_gamma = findViewById(R.id.et_angle_gamma);
+
+        TextView tv_side_a = findViewById(R.id.tv_side_a);
+        TextView tv_side_b = findViewById(R.id.tv_side_b);
+        TextView tv_side_c = findViewById(R.id.tv_side_c);
+        TextView tv_angle_alpha = findViewById(R.id.tv_angle_alpha);
+        TextView tv_angle_beta = findViewById(R.id.tv_angle_beta);
+        TextView tv_angle_gamma = findViewById(R.id.tv_angle_gamma);
 
 
         fab_calc.setOnClickListener(v -> {
@@ -41,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
             t.calcState();
 
+            tv_side_a.setText("Side a: " + round(t.getA(), 2));
+            tv_side_b.setText("Side b: " + round(t.getB(), 2));
+            tv_side_c.setText("Side c: " + round(t.getC(), 2));
+
+            tv_angle_alpha.setText("Angle α: " + round(t.getAlpha(), 2) + "°");
+            tv_angle_beta.setText("Angle β: " + round(t.getBeta(), 2) + "°");
+            tv_angle_gamma.setText("Angle γ: " + round(t.getGamma(), 2) + "°");
+
             Log.d("Side a is: ", String.valueOf(t.getA()));
             Log.d("Side b is: ", String.valueOf(t.getB()));
             Log.d("Side c is: ", String.valueOf(t.getC()));
@@ -50,5 +71,13 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Angle gamma is: ", String.valueOf(t.getGamma()));
 
         });
+    }
+
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
